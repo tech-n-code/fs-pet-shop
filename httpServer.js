73bb01bec53174c3ed078536d099dd6aeef25b8d
Will Franceschini <>
2023-03-27 16:29:30 -0700

    added POST req to httpServer

--- httpServer.js
+++ httpServer.js
@@ -3,6 +3,7 @@
 const http = require('http');
 const fs = require("fs");
 const path = require("path");
+const url = require("url");
 
 const petsPath = path.join(__dirname, "pets.json");
 
@@ -62,6 +63,53 @@ function startServer() {
                 res.setHeader("Content-Type", "text/plain");
                 res.end("Not Found");
             }
+        } else if (req.method === "POST" && req.url === "/pets") {
+            let body = "";
+            req.on("data", function(chunk) {
+                body += chunk.toString();
+            });
+            req.on("end", function() {
+                let params = JSON.parse(body);
+                console.log(params);
+                let petAge = parseInt(params.age);
+                let petKind = params.kind;
+                let petName = params.name;
+                if (isNaN(petAge) || !petKind || !petName) {
+                    res.statusCode = 400;
+                    res.setHeader("Content-Type", "text/plain");
+                    res.end("Bad Request");
+                    return;
+                }
+                fs.readFile(petsPath, "utf-8", function(err, data) {
+                    if (err) {
+                        console.error(err);
+                        res.statusCode = 500;
+                        res.setHeader("Content-Type", "text/plain");
+                        res.end("Internal Server Error");
+                        return;
+                    }
+                    let pets = JSON.parse(data);
+                    let newPet = {
+                        age: Number(petAge),
+                        kind: petKind,
+                        name: petName
+                    };
+                    pets.push(newPet);
+                    let petsJSON = JSON.stringify(pets);
+                    fs.writeFile(petsPath, petsJSON, function(err) {
+                        if (err) {
+                            console.error(err);
+                            res.statusCode = 500;
+                            res.setHeader("Content-Type", "text/plain");
+                            res.end("Internal Server Error");
+                            return;
+                        }
+                        res.statusCode = 200;
+                        res.setHeader("Content-Type", "application/json");
+                        res.end(JSON.stringify(newPet));
+                     })
+                });
+            });
         } else {
             res.statusCode = 404;
             res.setHeader("Content-Type", "text/plain");
